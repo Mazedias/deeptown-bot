89aabb957661bdd21fcdd8b65c9fd7d6f43d9913
Mathias <mawemw5@gmail.com>
2024-10-03 12:03:18 +0200

    Added method to calculate active players of a guild

--- src/api/guild_data.rs
+++ src/api/guild_data.rs
@@ -1,10 +1,11 @@
-use reqwest::{self, Response};
+use reqwest;
 use serde_json::Value;
 
 use crate::util::web_errors::WebRequestError;
 
 
 /// Calculates the amount of inactive players in the last/current event of a guild
+/// Active players are players that that donated in the last/current event.
 /// 
 /// # Arguments
 /// 
@@ -14,7 +15,6 @@ use crate::util::web_errors::WebRequestError;
 /// 
 /// On success returns the amount of inactive players else a `util::web_errors::WebRequestError`
 pub fn calculate_inactive_players(guild_id: i32) -> Result<i32, WebRequestError> {
-    
     let http_response = request_guild_data(guild_id)
         .map_err(|e| WebRequestError {
             code: 100,
@@ -42,6 +42,44 @@ pub fn calculate_inactive_players(guild_id: i32) -> Result<i32, WebRequestError>
     
 
     Ok(inactive_counter as i32)
+}
+
+
+/// Calculates the amount of active players in the last/current event of a guild.
+/// Active players are players that that donated in the last/current event.
+/// 
+/// # Arguments
+/// 
+/// * `guild_id` - ID of the guild at DeepTown Admin Tools
+/// 
+/// # Return
+/// 
+/// On success returns the amount of active players else a `util::web_errors::WebRequestError`
+pub fn calulate_active_players(guild_id: i32) -> Result<i32, WebRequestError> {
+    let http_response = request_guild_data(guild_id)
+        .map_err(|e| WebRequestError {
+            code: 100,
+            message: e.to_string(),
+        })?;
+
+    let players = http_response["players"]["data"]
+        .as_array()
+        .ok_or_else(|| WebRequestError {
+            code: 100,
+            message: "Players data not found or in unexpected format".to_string(),
+        })?;
+    
+    let active_counter = players
+        .iter()
+        .filter(|player| {
+            player.get(13)
+                .and_then(Value::as_u64)  
+                .map(|donations| donations != 0)
+                .unwrap_or(false)
+        })
+        .count();
+
+    Ok(active_counter as i32)
 }
 
 
